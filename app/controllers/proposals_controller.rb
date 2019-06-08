64f09f8eb89bc5e01184d3a8da75f55dc53e8054
jasonmiles <writejasonmiles@icloud.com>
2019-06-08 11:07:03 +0800

    Initial commit

---
+++ app/controllers/proposals_controller.rb
@@ -0,0 +1,48 @@
+class ProposalsController < ApplicationController
+  def index
+    @proposals = Proposal.all
+  end
+
+  def show
+    @proposal = Proposal.find(params[:id])
+  end
+
+  def new
+    @proposal = Proposal.new
+  end
+
+  def edit
+    @proposal = Proposal.find(params[:id])
+  end
+  def create
+    @proposal = current_user.proposals.new(proposal_params)
+    if @proposal.save
+      redirect_to @proposal
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @proposal = Proposal.find(params[:id])
+
+    if @proposal.update(proposal_params)
+      redirect_to @proposal
+    else
+      render 'edit'
+    end
+  end
+
+
+  def destroy
+    @proposal = Proposal.find(params[:id])
+    @proposal.destroy
+
+    redirect_to proposals_path
+  end
+
+  private
+    def proposal_params
+      params.require(:proposal).permit(:title, :text)
+    end
+end
---
+++ app/models/proposal.rb
@@ -0,0 +1,5 @@
+class Proposal < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+end
